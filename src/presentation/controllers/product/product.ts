74842631d3e1d49bbeb4cd172168ff3fa0ecc09b
jeseias <domingosjeseias@gmail.com>
2021-12-03 15:50:47 +0100

    feat: ensure ProductController returns 500 if AddProduct throws

--- src/presentation/controllers/product/product.ts
+++ src/presentation/controllers/product/product.ts
@@ -1,5 +1,5 @@
 import { AddProduct } from '../../../domain/usecases/add-product'
-import { ok } from '../../helpers/http/http-helper'
+import { ok, serverError } from '../../helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class ProductController {
@@ -8,12 +8,16 @@ export class ProductController {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, image, description } = httpRequest.body
-    const account = await this.addProduct.add({
-      name,
-      image,
-      description
-    })
-    return ok(account)
+    try {
+      const { name, image, description } = httpRequest.body
+      const account = await this.addProduct.add({
+        name,
+        image,
+        description
+      })
+      return ok(account)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
